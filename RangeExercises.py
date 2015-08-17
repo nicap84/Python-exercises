acf9a2a95d343e55b5b30ef90077649b12dd1678
nicap84 <mtmarin84@gmail.com>
2015-08-17 22:39:10 +0200

    Add exercises about range()

---
+++ RangeExercises.py
@@ -0,0 +1,31 @@
+__author__ = 'MariaII'
+
+
+# -*- encoding: utf-8 -*-
+
+def main():
+    exercise4()
+
+
+def exercise1():
+    # Print the sequence [0, 1, 2, ... 100]
+    print range(101)
+
+
+def exercise2():
+    # Print the sequence [0, 2, 4, ... 200]
+    print range(0, 201, 2)
+
+
+def exercise3():
+    # Print the sequence [86, 84, 82, ... 14]
+    print range(86, 13, -2)
+
+
+def exercise4():
+    # Print the sequence [10, 9, 8, ... 0]
+    print range(10, -1, -1)
+
+
+if __name__ == '__main__':
+    main()
